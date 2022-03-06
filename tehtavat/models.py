be6de764117a381ba89d3663ecf9365b622b7620
Anna44856 <44856@edu.raseko.fi>
2022-03-06 10:42:31 +0200

    14.02.2022

---
+++ tehtavat/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Kategoria(models.Model):
+    nimi = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nimi
+
+
+class Tehtava(models.Model):
+    otsikko = models.CharField(max_length=200)
+    kategoria = models.ForeignKey(
+        Kategoria,
+        on_delete=models.RESTRICT,
+        null=True,
+    )
+
+    # on_delete-säännöt
+    # 
+    # CASCADE: Kun kategoria poistetaan,
+    #   niin poistetaan kaikki sen tehtävät
+    # RESTRICT: Kategoriaa ei anneta poistaa
+    #   ennen kuin tehtävät on (käsin) poistettu
+    # SET_NULL: Jos kategoria poistettan,
+    #   niin sen kaikki tehtävät saavat kategorian arvoksi oletusarvo
+    # SET_DEFAULT: JOS kategoria poistetaan,
+    #   niin sen kaikkitehtävät saavat kategorian arvoksi oletusarvo
+
+
+    def __str__(self):
+        return self.otsikko
+
